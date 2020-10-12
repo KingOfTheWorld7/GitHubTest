2f4629f424e3324246993a22be3a7a9ab285222b
wenwen <2545027267@qq.com>
2020-10-12 16:51:37 +0800

    this is my third commit

--- GitHubt/src/main/java/com/offcn/github/User.java
+++ GitHubt/src/main/java/com/offcn/github/User.java
@@ -1,6 +1,10 @@
 package com.offcn.github;
 
 public class User {
-    private String name ;
+    private static String name ;
     private int age ;
+
+    public static void main(String[] args) {
+        System.out.println(name);
+    }
 }
